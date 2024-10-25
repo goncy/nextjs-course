6c027efdf6be98a41d5938a0f0040068ff51efae
goncy <gonzalo.pozzo4@gmail.com>
2024-10-25 12:30:50 -0300

    Update readme

--- code/next.config.ts
+++ code/next.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   experimental: {
-    ppr: false,
     reactCompiler: true,
   },
   logging: {
